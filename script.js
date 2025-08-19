7b68aae77e07b85860e1ba59e83b8e0f5452835f
Tiago Amaral <tiagoamaraltic@gmail.com>
2025-08-18 22:17:20 -0300

    copy button

--- script.js
+++ script.js
@@ -1,79 +1,98 @@
-const ativarOption = document.querySelectorAll('.option');
+  const ativarOption = document.querySelectorAll('.option');
 
-function addClass(event) {
+  function addClass(event) {
+    ativarOption.forEach(function(option) {
+      option.classList.remove("option-ativo");
+    });
+
+    event.target.classList.add("option-ativo");
+  }
+
   ativarOption.forEach(function(option) {
-    option.classList.remove("option-ativo");
+    option.addEventListener('click', addClass);
   });
 
-  event.target.classList.add("option-ativo");
-}
 
-ativarOption.forEach(function(option) {
-  option.addEventListener('click', addClass);
+
+
+
+  const rangeCustom = document.querySelector('.range');
+  const numberCustom = document.querySelector('.number');
+
+
+  rangeCustom.max = 20;
+
+  function syncRangeWithNumber() {
+    numberCustom.value = rangeCustom.value;
+  }
+
+  function syncNumberWithRange() {
+    rangeCustom.value = numberCustom.value;
+  }
+
+  rangeCustom.addEventListener('input', syncRangeWithNumber);
+
+  numberCustom.addEventListener('input', syncNumberWithRange);
+
+
+
+
+
+  const checkboxUpper = document.querySelector('.c1 input');
+  const checkboxLower = document.querySelector('.c2 input');
+  const checkboxNumbers = document.querySelector('.c3 input');
+  const checkboxSymbols = document.querySelector('.c4 input');
+  const rangeInput = document.querySelector('.range');
+  const numberInput = document.querySelector('.number');
+  const resultInput = document.querySelector('.result input');
+  const generateButton = document.querySelector('.generate-btn');
+
+  function generatePassword() {
+    let characters = ""; 
+
+    if (checkboxUpper.checked) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (checkboxLower.checked) characters += "abcdefghijklmnopqrstuvwxyz";
+    if (checkboxNumbers.checked) characters += "0123456789";
+    if (checkboxSymbols.checked) characters += "!@#$%^&*()_+-=[]{}|;:'\",.<>?/";
+
+    if (characters === "") {
+      alert("Selecione pelo menos uma opção para a senha.");
+      return "";
+    }
+
+    const passwordLength = parseInt(numberInput.value); 
+
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      password += characters[randomIndex];
+    }
+
+    return password;
+  }
+
+  function displayGeneratedPassword() {
+    const password = generatePassword();
+    if (password) resultInput.value = password; 
+  }
+
+  generateButton.addEventListener('click', displayGeneratedPassword);
+
+
+const copyButton = document.querySelector('.result button');
+copyButton.addEventListener('click', () => {
+  const password = resultInput.value;
+
+  if (!password) {
+    alert("Nenhuma senha para copiar!");
+    return;
+  }
+
+  navigator.clipboard.writeText(password)
+    .then(() => {
+      alert("Senha copiada com sucesso!");
+    })
+    .catch(err => {
+      alert("Erro ao copiar a senha: " + err);
+    });
 });
-
-
-
-
-
-const rangeCustom = document.querySelector('.range');
-const numberCustom = document.querySelector('.number');
-
-
-rangeCustom.max = 30;
-
-function syncRangeWithNumber() {
-  numberCustom.value = rangeCustom.value;
-}
-
-function syncNumberWithRange() {
-  rangeCustom.value = numberCustom.value;
-}
-
-rangeCustom.addEventListener('input', syncRangeWithNumber);
-
-numberCustom.addEventListener('input', syncNumberWithRange);
-
-
-
-
-
-const checkboxUpper = document.querySelector('.c1 input');
-const checkboxLower = document.querySelector('.c2 input');
-const checkboxNumbers = document.querySelector('.c3 input');
-const checkboxSymbols = document.querySelector('.c4 input');
-const rangeInput = document.querySelector('.range');
-const numberInput = document.querySelector('.number');
-const resultInput = document.querySelector('.result input');
-const generateButton = document.querySelector('.generate-btn');
-
-function generatePassword() {
-  let characters = ""; 
-
-  if (checkboxUpper.checked) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (checkboxLower.checked) characters += "abcdefghijklmnopqrstuvwxyz";
-  if (checkboxNumbers.checked) characters += "0123456789";
-  if (checkboxSymbols.checked) characters += "!@#$%^&*()_+-=[]{}|;:'\",.<>?/";
-
-  if (characters === "") {
-    alert("Selecione pelo menos uma opção para a senha.");
-    return "";
-  }
-
-  const passwordLength = parseInt(numberInput.value); 
-
-  let password = "";
-  for (let i = 0; i < passwordLength; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    password += characters[randomIndex];
-  }
-
-  return password;
-}
-
-function displayGeneratedPassword() {
-  const password = generatePassword();
-  if (password) resultInput.value = password; 
-}
-
-generateButton.addEventListener('click', displayGeneratedPassword);
